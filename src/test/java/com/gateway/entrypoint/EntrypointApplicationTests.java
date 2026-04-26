900f1d5a3d7634d6dd4bb9bb951f129b507a3253
asadibharadwaj <bbharadwaj7@gmail.com>
2026-04-26 14:25:43 +0530

    First commit

---
+++ src/test/java/com/gateway/entrypoint/EntrypointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.entrypoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrypointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
